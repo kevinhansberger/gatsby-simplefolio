49e8a1338786dc982488c4067e548efed2c93b66
Kevin Hansberger <kev@thehansbergers.com>
2021-05-09 21:33:21 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Kevin Hansberger | Engage • Educate • Empower'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Kevin Hansberger is a follower of Jesus Christ, a marketing consultant, and a political consultant.'} />
       </Helmet>
       <App />
     </>
